1b2c59d3a32b713eecc14f038a4fa67fbd942271
aron-francis <aron.francis@yahoo.com>
2024-09-09 18:23:14 +0200

    pre-final

--- terraform-demo1/main.tf
+++ terraform-demo1/main.tf
@@ -1,5 +1,29 @@
+# Variable declarations
+variable "ec2_count" {
+  description = "Number of EC2 instances to create"
+  type        = number
+  default     = 1
+}
+
+variable "rds_count" {
+  description = "Number of RDS instances to create"
+  type        = number
+  default     = 1
+}
+
 provider "aws" {
   region = "eu-central-1"
+}
+
+# Data source for Amazon Linux 2 AMI
+data "aws_ami" "amazon_linux_2" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
 }
 
 # VPC
@@ -69,6 +93,7 @@ resource "aws_security_group" "demo_sg" {
 
 # EC2 Instance
 resource "aws_instance" "demo_instance" {
+  count         = var.ec2_count
   ami           = data.aws_ami.amazon_linux_2.id
   instance_type = "t3.micro"
   subnet_id     = aws_subnet.demo_subnet_1.id
@@ -81,7 +106,7 @@ resource "aws_instance" "demo_instance" {
   }
 
   tags = {
-    Name = "demo-instance"
+    Name = "demo-instance-${count.index + 1}"
   }
 }
 
@@ -97,10 +122,11 @@ resource "random_id" "suffix" {
 }
 
 resource "aws_db_instance" "demo_db" {
+  count                = var.rds_count
   allocated_storage    = 20
   engine               = "mysql"
   instance_class       = "db.t3.micro"
-  db_name              = "mydb"
+  db_name              = "mydb${count.index + 1}"
   username             = "admin"
   password             = "password"
   vpc_security_group_ids = [aws_security_group.demo_sg.id]
@@ -108,4 +134,16 @@ resource "aws_db_instance" "demo_db" {
   publicly_accessible   = true
   multi_az              = false
   db_subnet_group_name  = aws_db_subnet_group.demo_subnet_group.name
+  identifier            = "demo-db-${count.index + 1}-${random_id.suffix.hex}"
+}
+
+# Add outputs for EC2 and RDS instances
+output "ec2_instance_ids" {
+  description = "IDs of created EC2 instances"
+  value       = aws_instance.demo_instance[*].id
+}
+
+output "rds_endpoints" {
+  description = "Endpoints of created RDS instances"
+  value       = aws_db_instance.demo_db[*].endpoint
 }
